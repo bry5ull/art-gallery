b4fe1516ca0bcceaf72bb99df0aabf8ca88dc687
bry5ull <bry@bry5ull.dev>
2022-06-16 20:31:45 -0500

    up2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react'
+import Gallery from './gallery/gallery'
+import Buttons from './gallery/buttons'
+
 
 function App() {
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+  
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value))
+  }
+  useEffect(() => {
+      document.title='Welcome to Artworld'
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId])
+
+const displayImage = () => {
+  if(!data.primaryImage) {
+    return (
+      <h2>No Image!</h2>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Gallery objectImg={data.primaryImage} title={data.title} />
+  )
+}
+  return (
+    <><div className='App'>
+      <h1>{data.title}</h1>
+      {displayImage()}
+    </div><div>
+        <Buttons handleIterate={handleIterate} />
+      </div></>
   );
 }
 
-export default App;
+export default App
